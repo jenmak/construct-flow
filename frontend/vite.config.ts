3a574f1856b75779fb731eb8fc7b62dd453a0ceb
Jen Mak <jennifer.michelle.mak@gmail.com>
2025-10-23 15:49:58 -0400

    Fixes to console errors

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      jsxImportSource: "react"
-    }),
+    react(),
     tailwindcss()
   ],
   clearScreen: false,
