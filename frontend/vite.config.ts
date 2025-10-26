f878fb2f9247aa2c0015a3ca918feb0c18686a89
Jen Mak <jennifer.michelle.mak@gmail.com>
2025-10-26 06:34:54 -0400

    BACK TO DRIP DROP WINNING FORMULA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    allowedHosts: ["all"]
+    allowedHosts: "all"
   },
   resolve: {
     alias: {
